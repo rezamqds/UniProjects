203e37d502b37941a2deafa62306f72baed939bf
rezamqds <rezamqds@gmail.com>
2022-12-21 23:41:05 +0330

    added score and var in score high score \ print in turtle screen ! + reForm Code

--- SnakeGame/main.py
+++ SnakeGame/main.py
@@ -1,30 +1,30 @@
-#import modules
+# import modules
 import turtle as tr
 import time
 import random as rand
-#init score and high score
+
+# init score and high score
 score = 0
 high_score = 0
-#make turtle setup
+# make turtle setup
 tr.bgcolor('black')
 
-#score print
+# score print
 sc = tr.Turtle()
-sc.speed(0)
 sc.shape("square")
 sc.color("white")
 sc.penup()
 sc.hideturtle()
 sc.goto(0, 350)
-sc.write("Score : %d  High Score : %d"%(score,high_score), align="center",font=("candara", 24, "bold"))
+sc.write("Score : %d  High Score : %d" % (score, high_score), align="center", font=("Arial", 24, 'normal'))
 
-#snake setup
+# snake setup
 snake = tr.Turtle()
 snake.penup()
 snake.shape("square")
 snake.color("cyan")
 
-#food setup
+# food setup
 food = tr.Turtle()
 food.speed(0)
 food.shape('circle')
