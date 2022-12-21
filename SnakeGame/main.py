6573a85b5da60f43b2708dae40d909f91bdbe26f
rezamqds <rezamqds@gmail.com>
2022-12-21 23:01:20 +0330

    f

--- SnakeGame/main.py
+++ SnakeGame/main.py
@@ -1,12 +1,16 @@
 #import modules
 import turtle as tr
-import time as tm
+import time
 import random as rand
-
+#init score and high score
 score = 0
 high_score = 0
+#make turtle setup
 tr.bgcolor('cyan')
 tr.shape('square')
 tr.color('red')
+tr.goto(50,40)
 target=tr.Turtle()
+tr.goto(-50,40)
+target.goto(40,100)
 tr.done()
