c5b0d3e1246f5adc108092a8ff4cacd4ba48561d
rezamqds <rezamqds@gmail.com>
2022-12-21 22:56:08 +0330

    added main file

---
+++ SnakeGame/main.py
@@ -0,0 +1,12 @@
+#import modules
+import turtle as tr
+import time as tm
+import random as rand
+
+score = 0
+high_score = 0
+tr.bgcolor('cyan')
+tr.shape('square')
+tr.color('red')
+target=tr.Turtle()
+tr.done()
