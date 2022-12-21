028db27be8cb6afa9d6ee024721f9dec65f5169f
rezamqds <rezamqds@gmail.com>
2022-12-21 23:29:40 +0330

    added score and var in score high score \ print in turtle screen !

--- SnakeGame/main.py
+++ SnakeGame/main.py
@@ -7,10 +7,29 @@ score = 0
 high_score = 0
 #make turtle setup
 tr.bgcolor('black')
+
+#score print
+sc = tr.Turtle()
+sc.speed(0)
+sc.shape("square")
+sc.color("white")
+sc.penup()
+sc.hideturtle()
+sc.goto(0, 350)
+sc.write("Score : %d  High Score : %d"%(score,high_score), align="center",font=("candara", 24, "bold"))
+
 #snake setup
 snake = tr.Turtle()
 snake.penup()
 snake.shape("square")
 snake.color("cyan")
+
+#food setup
+food = tr.Turtle()
+food.speed(0)
+food.shape('circle')
+food.color('green')
+food.penup()
+food.goto(-68, 135)
 
 tr.done()
