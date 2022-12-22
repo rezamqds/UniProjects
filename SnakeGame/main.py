e7544de38bb3d3c86303b328186c2f3bbf69236a
rezamqds <rezamqds@gmail.com>
2022-12-22 19:25:56 +0330

    set speed to zero for no lagging!

--- SnakeGame/main.py
+++ SnakeGame/main.py
@@ -8,10 +8,10 @@ score = 0
 high_score = 0
 # make turtle setup
 tr.bgcolor('black')
-
+tr.speed(0)
 # score print
 sc = tr.Turtle()
-sc.shape("square")
+sc.speed(0)
 sc.color("white")
 sc.penup()
 sc.hideturtle()
@@ -20,6 +20,7 @@ sc.write("Score : %d  High Score : %d" % (score, high_score), align="center", fo
 
 # snake setup
 snake = tr.Turtle()
+snake.speed(0)
 snake.penup()
 snake.shape("square")
 snake.color("cyan")
@@ -31,5 +32,7 @@ food.shape('circle')
 food.color('green')
 food.penup()
 food.goto(-68, 135)
+
+
 
 tr.done()
