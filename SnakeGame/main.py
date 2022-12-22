7850b947ed8a0f9ed5596f81e85a5c8140d07d9b
rezamqds <rezamqds@gmail.com>
2022-12-22 21:50:38 +0330

    Fix direction

--- SnakeGame/main.py
+++ SnakeGame/main.py
@@ -7,20 +7,19 @@ import random as rand
 score = 0
 high_score = 0
 print("Hello Welcome to @rezamqds 's snake game \n")
-x=int(input("!!! Please enter width(X) !!! : "))
-y=int(input("!!! Please enter height(Y) !!! : "))
-if x==1337:
-    score=9999
-    high_score=9999
-    x=850
-    y=850
-    God=True
+x = int(input("!!! Please enter width(X) !!! : "))
+y = int(input("!!! Please enter height(Y) !!! : "))
+if x == 1337:
+    score = 9999
+    high_score = 9999
+    x = 850
+    y = 850
+    God = True
 # make turtle setup
 screen = tr.Screen()
 screen.setup(width=x, height=y)
 screen.bgcolor('black')
 screen.title("@rezamqds | Snake Game")
-
 
 # score print
 sc = tr.Turtle()
@@ -30,14 +29,16 @@ sc.penup()
 sc.hideturtle()
 sc.goto(0, 350)
 sc.write("Score : %d  High Score : %d" % (score, high_score), align="center", font=("Arial", 24, 'normal'))
-sc.goto(300,-200)
+sc.goto(300, -200)
 sc.write("GOD mode ;)")
+
 # snake setup
 snake = tr.Turtle()
 snake.speed(0)
 snake.penup()
 snake.shape("square")
 snake.color("cyan")
+snake.direction = "Stop"
 
 # food setup
 food = tr.Turtle()
@@ -53,15 +54,12 @@ def move():
     if snake.direction == "up":
         y = snake.ycor()
         snake.sety(y + 2)
-
     if snake.direction == "down":
         y = snake.ycor()
         snake.sety(y - 2)
-
     if snake.direction == "right":
         x = snake.xcor()
         snake.setx(x + 2)
-
     if snake.direction == "left":
         x = snake.xcor()
         snake.setx(x - 2)
@@ -72,4 +70,3 @@ while True:
     tr.update()
 
 tr.done()
-
