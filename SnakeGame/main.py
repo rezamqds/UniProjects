cdde8cac710e19e9c3a8b322c38b2cec43e0b9fe
rezamqds <rezamqds@gmail.com>
2022-12-24 14:05:16 +0330

    correct listen place

--- SnakeGame/main.py
+++ SnakeGame/main.py
@@ -40,19 +40,19 @@ if GOD == True:
     sc.write("GOD mode ;)")
 
 # Game Border
-border=tr.Turtle()
-w=400
+border = tr.Turtle()
+w = 400
 border.speed(8)
 border.penup()
 border.hideturtle()
-border.goto(w,w)
+border.goto(w, w)
 border.color('red')
 border.pendown()
 border.pensize(20)
-border.goto(w,-w)
-border.goto(-w,-w)
-border.goto(-w,w)
-border.goto(w,w)
+border.goto(w, -w)
+border.goto(-w, -w)
+border.goto(-w, w)
+border.goto(w, w)
 
 # snake setup
 snake = tr.Turtle()
@@ -69,58 +69,61 @@ food.shape('circle')
 food.color('green')
 food.penup()
 randFood = rand.randint(5, 350)
-food.goto(randFood,randFood)
+food.goto(randFood, randFood)
+
+
 # def for angle of snake
 
 def up():
     snake.setheading(90)
+
+
 def down():
     snake.setheading(270)
+
+
 def left():
     snake.setheading(180)
+
+
 def right():
     snake.setheading(0)
 
+# Move with arrow keys
+screen.listen()
+screen.onkey(up, 'Up')
+screen.onkey(down, 'Down')
+screen.onkey(left, 'Left')
+screen.onkey(right, 'Right')
+
+screen.listen()
+screen.onkeypress(up, 'w')
+screen.onkeypress(down, "s")
+screen.onkeypress(left, "a")
+screen.onkeypress(right, "d")
+
 speed = 3
 sc.goto(0, 350)
-# set conditions
-cond = False
+
 consize = 380
-while not cond:
+while True:
     screen.update()
     snake.forward(speed)
-
-    # Move with arrow keys
-    screen.listen()
-    screen.onkey(up, 'Up')
-    screen.onkey(down, 'Down')
-    screen.onkey(left, 'Left')
-    screen.onkey(right, 'Right')
-
-    screen.listen()
-    screen.onkeypress(up,'w')
-    screen.onkeypress(down,"s")
-    screen.onkeypress(left,"a")
-    screen.onkeypress(right,"d")
 
     while snake.distance(food) < 20:
         randFood = rand.randint(5, 350)
         food.goto(randFood, randFood)
-        score+=5
+        score += 5
         if score > high_score:
             high_score = score
         sc.clear()
         sc.write("Score : %d  High Score : %d" % (score, high_score), align="center", font=("Arial", 24, 'normal'))
-
-
 
     # Game over cond
     if snake.xcor() > consize or snake.xcor() < -consize or snake.ycor() > consize or snake.ycor() < -consize:
         cond = True
         snake.color('red')
         snake.goto(0, 0)
-        #after Game over
+        # after Game over
         sc.goto(0, 10)
         sc.write("GAME OVER")
-
-
