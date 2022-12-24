a35e4a5d939cd1f238970139fb13b8aef9f90bf7
rezamqds <rezamqds@gmail.com>
2022-12-24 14:42:48 +0330

    added snake 2 and 3 and segments = []

--- SnakeGame/main.py
+++ SnakeGame/main.py
@@ -60,7 +60,24 @@ snake.speed(0)
 snake.penup()
 snake.shape("square")
 snake.color("cyan")
-snake.direction = "Stop"
+
+# snake2 +
+snake2 = tr.Turtle()
+snake2.speed(0)
+snake2.penup()
+snake2.shape("square")
+snake2.color("cyan")
+snake2.goto(1000, 2000)
+
+# snake3 +
+snake3 = tr.Turtle()
+snake3.speed(0)
+snake3.penup()
+snake3.shape("square")
+snake3.color("cyan")
+snake3.goto(1000, 2000)
+
+
 
 # food setup
 food = tr.Turtle()
@@ -77,7 +94,6 @@ food.goto(randFood, ranF)
 
 def up():
     snake.setheading(90)
-
 
 def down():
     snake.setheading(270)
@@ -106,7 +122,7 @@ screen.onkeypress(right, "d")
 
 speed = 3
 ScoreW.goto(0, 350)
-
+segments = []
 conSize = 380
 
 while True:
@@ -118,11 +134,19 @@ while True:
         ranF = rand.randint(-350, 350)
         food.goto(randFood, ranF)
 
+        while True:
+            x = snake.xcor()
+            y = snake.ycor()
+            snake2.goto(x - 10, y - 10)
+            break
+
+        # Score +
         score += 5
         if score > high_score:
             high_score = score
         ScoreW.clear()
         ScoreW.write("Score : %d  High Score : %d" % (score, high_score), align="center", font=("Arial", 24, 'normal'))
+
 
     # Game over cond
     if snake.xcor() > conSize or snake.xcor() < -conSize or snake.ycor() > conSize or snake.ycor() < -conSize:
@@ -134,7 +158,7 @@ while True:
         ScoreW.write("GAME OVER")
         time.sleep(1.5)
         ScoreW.goto(0, 350)
-        score=0
+        score = 0
         ScoreW.clear()
         ScoreW.write("Score : %d  High Score : %d" % (score, high_score), align="center", font=("Arial", 24, 'normal'))
         snake.color('cyan')
