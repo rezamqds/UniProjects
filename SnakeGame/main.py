63f4428b4e60dd064747ebe3640b89a235528b68
rezamqds <rezamqds@gmail.com>
2022-12-24 13:25:50 +0330

    game over not fixed!

--- SnakeGame/main.py
+++ SnakeGame/main.py
@@ -40,19 +40,19 @@ if GOD == True:
     sc.write("GOD mode ;)")
 
 # Game Border
-border=tr.Turtle()
-w=400
+border = tr.Turtle()
+w = 400
 border.speed(8)
 border.penup()
 border.hideturtle()
-border.goto(w,w)
+border.goto(w, w)
 border.color('red')
 border.pendown()
 border.pensize(20)
-border.goto(w,-w)
-border.goto(-w,-w)
-border.goto(-w,w)
-border.goto(w,w)
+border.goto(w, -w)
+border.goto(-w, -w)
+border.goto(-w, w)
+border.goto(w, w)
 
 # snake setup
 snake = tr.Turtle()
@@ -60,7 +60,6 @@ snake.speed(0)
 snake.penup()
 snake.shape("square")
 snake.color("cyan")
-snake.direction = "Stop"
 
 # food setup
 food = tr.Turtle()
@@ -68,59 +67,67 @@ food.speed(0)
 food.shape('circle')
 food.color('green')
 food.penup()
-randFood = rand.randint(5, 350)
-food.goto(randFood,randFood)
-# def for angle of snake
+randFood = rand.randint(-350, 350)
+food.goto(randFood, randFood)
 
+
+# def for angle of snake
 def up():
     snake.setheading(90)
+
+
 def down():
     snake.setheading(270)
+
+
 def left():
     snake.setheading(180)
+
+
 def right():
     snake.setheading(0)
 
+
+# add segment list
+segments = []
+
 speed = 3
 sc.goto(0, 350)
-# set conditions
-cond = False
-consize = 380
-while not cond:
+
+# Move with arrow keys
+screen.listen()
+screen.onkey(up, 'Up')
+screen.onkey(down, 'Down')
+screen.onkey(left, 'Left')
+screen.onkey(right, 'Right')
+# Move with WASD
+screen.onkeypress(up, 'w')
+screen.onkeypress(down, "s")
+screen.onkeypress(left, "a")
+screen.onkeypress(right, "d")
+
+# main Game
+conSize = 380
+while True:
     screen.update()
     snake.forward(speed)
 
-    # Move with arrow keys
-    screen.listen()
-    screen.onkey(up, 'Up')
-    screen.onkey(down, 'Down')
-    screen.onkey(left, 'Left')
-    screen.onkey(right, 'Right')
-
-    screen.listen()
-    screen.onkeypress(up,'w')
-    screen.onkeypress(down,"s")
-    screen.onkeypress(left,"a")
-    screen.onkeypress(right,"d")
-
     while snake.distance(food) < 20:
-        randFood = rand.randint(5, 350)
-        food.goto(randFood, randFood)
-        score+=5
+        randFood = rand.randint(-350, 350)
+        yrf = rand.randint(-350, 350)
+        food.goto(randFood, yrf)
+        score += 5
+
         if score > high_score:
             high_score = score
+
         sc.clear()
         sc.write("Score : %d  High Score : %d" % (score, high_score), align="center", font=("Arial", 24, 'normal'))
 
-
-
     # Game over cond
-    if snake.xcor() > consize or snake.xcor() < -consize or snake.ycor() > consize or snake.ycor() < -consize:
-        cond = True
+    if snake.xcor() > conSize or snake.xcor() < -conSize or snake.ycor() > conSize or snake.ycor() < -conSize:
         snake.color('red')
         snake.goto(0, 0)
-        #after Game over
+        # after Game over
         sc.goto(0, 10)
         sc.write("GAME OVER")
-
-
