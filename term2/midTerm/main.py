47d3907ed45b596f27088df5abedde5b01140882
rezamqds <85675968+rezamqds@users.noreply.github.com>
2023-04-02 14:57:38 +0330

    Update main.py
    completed

--- term2/midTerm/main.py
+++ term2/midTerm/main.py
@@ -1,4 +1,5 @@
 import check_ath, viewDrug, editDrug, checkDName
+from os import system as littleSkiny
 from clsClear import clear as cl3
 from time import sleep
 
@@ -17,11 +18,11 @@ while not logedIn:
     
         except ValueError:
             cl3()
-            print("\t\t!!!! Invalid input. Please enter Numbers !!!!")
+            print("\n\t\t!!!! Invalid input. Please enter Numbers !!!!\n")
 
 
         else:
-            # add try except for each optins ==66== !!!!!
+            # add try except for each optins ==00== checked/
             if loginFlag == 1 :
                 cl3()
                 print("\t\tHello manager!")
@@ -29,22 +30,30 @@ while not logedIn:
                 chk = check_ath.checkAth(UID)
 
                 # cond for debug :
-                chk = True
+                # chk = True
                 # end dg
+                errShown = False
 
                 while chk:
                     sleep(1)
                     cl3()
                     print(" Choose Admin list :")
                     sleep(0.2)
-                    print("1. view drugs\n2. edit drugs\n3. any idea?")
-                    listChoose = int(input("----> "))
-                    
+                    print("1. view drugs\n2. edit drugs\n3. ExitAdmin")  #if freetime: option for change admin pass
+                    try :
+                        listChoose = int(input("----> "))
+                    except ValueError:
+                        errShown = True
+                        print("\t\t!!!!Please enter Numbers !!!!")
+                        sleep(1)
+                        cl3()
+
+                    # else:
                     if listChoose == 1:
                         cl3()            
                         print("Drugs list : ")
                         sleep(0.5)
-                        viewDrug.showList()
+                        viewDrug.showList(True)
                         leave = input("Do you want to beck to menu ? y/n ")
                         # ==00== condition in while stay or leave --  if leave nothing it takes back to menu, if stay sleep
                         # just handle the the logic
@@ -60,7 +69,7 @@ while not logedIn:
 
                     elif listChoose == 2:
                         cl3()
-                        lizza = int(input("Do you want \n1.Add \n2.Replace \n3.Remove \n----> "))
+                        lizza = int(input("\Edit drugs :\n~~~~ \n0.\BackMenu/ \n1.Add \n2.Replace \n3.Remove \n4.EditEntityCount\n~~~~ \n----> "))
                         if lizza == 1 :
                             # Add ...
                             isTrue = False
@@ -77,7 +86,7 @@ while not logedIn:
                                 isTrue = input(" y / n ?")
                                 cl3()
                                 if isTrue.lower() == 'y':
-                                    # it should be saved in the lists and then BREAKKKK -- checcked =00=
+                                    # it should be saved in the lists and then BREAKKKK -- checcked =00= /
                                     editDrug.addList(newDrug)
                                     sleep(2.5)
                                     # pass
@@ -86,20 +95,31 @@ while not logedIn:
                                     cl3()
                                     print("\tyou can enter thar again ...")
                             
-                            print("taking back to main menu ...")
-                            sleep(1.6)
-
-                        elif lizza == 2 :
-                            # Replace ...
-                            viewDrug.showList()
-                            repDrugNum = int(input("\n--->Enter the number of drugs that you wanna replace: "))
-                            repDrugName = checkDName.chkname()
-                            editDrug.repList(repDrugName, repDrugNum)
-                            print("Success!")
                             vw = input("Do you want to see new list? y/n ")
                             if vw == 'y':
                                 cl3()
-                                viewDrug.showList()
+                                viewDrug.showList(True)
+                                if input("back to menu? y/n ") == 'n' :
+                                    print("ss uptime 2 sec")
+                                    sleep(2)
+                                    
+                                sleep(1.5)
+
+                            else :
+                                print("Taking back to main menu ...")
+                                sleep(2)
+
+                        elif lizza == 2 :
+                            # Replace ...
+                            viewDrug.showList(True)
+                            repDrugNum = int(input("\n--->Enter the number of drugs that you wanna replace: "))
+                            repDrugName = checkDName.chkname()
+                            editDrug.repList(repDrugName, repDrugNum)
+                            print("Success! , Replaced!")
+                            vw = input("Do you want to see new list? y/n ")
+                            if vw == 'y':
+                                cl3()
+                                viewDrug.showList(True)
                                 if input("back to menu? y/n ") == 'n' :
                                     print("ss uptime 2 sec")
                                     sleep(2)
@@ -108,7 +128,7 @@ while not logedIn:
 
                         elif lizza == 3 :
                             # Remove
-                            viewDrug.showList()
+                            viewDrug.showList(True)
                             rmDrugNum = int(input("\n--->Enter the number of drugs that you wanna remove: "))
                             drugs = open('drugList.txt','r')
                             # lenD = len(drugs.readlines())
@@ -116,32 +136,155 @@ while not logedIn:
                             for i in drugs:
                                 co += 1
                                 if co == rmDrugNum:
-                                    surmnt = input()(f"Are you sure you wanna del {i}? y/n ")
+                                    surmnt = input(f"Are you sure you wanna del {i}? y/n ")
                                     break
                             if surmnt == "y":
                                                         #use the def for del the val !!!
-                                pass
+                                editDrug.remList(rmDrugNum)
+                                print("Success! , Removed!")
+                            vw = input("Do you want to see new list? y/n ")
+                            if vw == 'y':
+                                cl3()
+                                viewDrug.showList(True)
+                                if input("back to menu? y/n ") == 'n' :
+                                    print("ss uptime 2 sec")
+                                    sleep(2)
+                                    
+                                sleep(1.5)
+
                             else :
                                 print("Taking back to main menu ...")
-                                sleep(secs)
-                                
+                                sleep(2)
+                            drugs.close()
 
 
+                        elif lizza == 0 :
+                            print("back to main menu ...")
 
+                        elif lizza == 4 :
+                            # EditEntityCount
+                            viewDrug.showList(True)
+                            try:
+                                numbr = int(input("Please enter the number of drug: "))
+                                newTedad = int(input("please eneter the new entitiy count: "))
+                            except ValueError:
+                                print("NUMBERS!")
+                            else :
+                                editDrug.EditEntityCount(numbr, str(newTedad))
+                            sleep(2)
 
+                        else :
+                            print(f"{lizza} is not in the range ! ")
+                            sleep(1)
+                
 
+                    elif listChoose == 3:
+                        # 3 exitAdmin
+                        print("Safly close files and logout ADMIN!")
+                        sleep(1)
+                        chk = False
 
-
+                    elif not errShown :
+                        print('choose options in list range !')
+                        sleep(1)
 
                 else:
                     sleep(1.5)
                     cl3()
-                
 
             elif loginFlag == 2 :
-                cl3()
-                print("\t\tHello Customer what do you need?")
+                while True:
+                    cl3()
+                    print("\t\tHello Customer what do you need?")
+                    print("1. view drugs\n2. buy drugs\n3. Exit")
+                    try:
+                        listChoose = int(input("----> "))
+
+                    except ValueError:
+                        print("     Use numbers !!!")
+                        sleep(1)
+                        cl3()
+
+                    else:
+                        if listChoose == 1 :
+                            cl3()            
+                            print("Drugs list : ")
+                            sleep(0.5)
+                            viewDrug.showList(False)
+                            leave = input("Do you want to beck to menu ? y/n ")
+                            while leave.lower() == 'n' :
+                                print("your seassion reamining 3 sec more")
+                                sleep(3)
+                                leave = input("Do you want to beck to menu? y[or Enter] / n ")
+                        elif listChoose == 2 :
+                                                                            # buy drug
+                                
+                            print("Choose the drug num")
+                            sleep(1)
+
+                            viewDrug.showList(False)
+                            try:
+                                buyDrug = int(input("Drug num: "))
+                            except ValueError:
+                                print("enter NUMBERS!")
+                                sleep(secs)
+                            
+                            else:
+
+                                try:
+                                    cntit = int(input('how many do you want ? '))
+                                    sleep(0.2)
+                                    cl3()
+                                except ValueError:
+                                    # cl3()
+                                    print("we said how many? NUMBER is the awnser!")
+                                    sleep(1)
+                                else:
+                                    if cntit > 5 :
+                                        print('you are not allowd to pick more than 5 !')
+                                        print('max=5 , now is in your cart')
+                                        # kam kardan tedad daroo
+                                        editDrug.buyDrug(buyDrug, 5)
+                                    elif cntit < 1:
+                                        print("\t\tit looks like you are not OKAY! cannot buy less than one item")
+                                        print('taking back to the main menu ...')
+                                        sleep(0.8)
+                                        break
+
+                                    else:
+                                        # kam kardan tedad daroo                00000000
+                                        editDrug.buyDrug(buyDrug, cntit)
+                                        print('Here u are!\n')
+                                        # sleep(1)
+                                        
+
+                                try :
+                                    payWay = input("How would you pay?\n1.card\n2.cash\n--> ")
+                                
+                                except ValueError:
+                                    print("JUST NUMBERS!")
+                                
+                                else:
+                                    if payWay == 1:
+                                        pass
+                                    else :
+                                        print("after epidemic you just can use your card ... ")
+                                        sleep(1.5)
+                                    littleSkiny("start "+"rule3char.txt")
+
+
+                        elif listChoose == 3 :
+                            # exit
+                            cl3()
+                            print("Exit ...")
+                            sleep(1)
+                            cl3()
+                            break
+                        else:
+                            print("     Use the list range !!!")
+                            sleep(1.6)
+                            cl3()
             else:
                 cl3()
-                print("\t\t!!!! Please enter list numbers! Try Again !!!!")
+                print("\n\t\t!!!! Please enter list numbers! Try Again !!!!\n")
                 
