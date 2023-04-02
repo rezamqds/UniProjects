30054198bf3ab23a0a2ef9bec82ba6c69f681ed2
rezamqds <85675968+rezamqds@users.noreply.github.com>
2023-04-02 14:56:41 +0330

    Update viewDrug.py
    add privilege option for view quantity of drugs

--- term2/midTerm/viewDrug.py
+++ term2/midTerm/viewDrug.py
@@ -1,12 +1,30 @@
 from time import sleep
 
-def showList():
+def showList(prv):
     drugs = open('drugList.txt','r')
     dCount = 1
-    for this_drug in drugs:
-        sleep(0.18)
-        if dCount < 10 :
-            print(dCount,"  •",this_drug,end="")
-        else:    
-            print(dCount," •",this_drug,end="")
-        dCount +=1
+    if prv:
+        drugsEntit = open('drugEntity.txt').read()
+        drugsEntit = drugsEntit.split()
+        x=0
+        # print(drugsEntit)
+        for this_drug in drugs:
+            sleep(0.5)
+            if dCount < 10 :
+                print(dCount,"  •",this_drug,"\t↳ Count : ",drugsEntit[x])
+            else:    
+                print(dCount," •",this_drug,"\t↳ Count : ",drugsEntit[x])
+            dCount += 1
+            x += 1
+    else:
+        for this_drug in drugs:
+            sleep(0.18)
+            if dCount < 10 :
+                print(dCount,"  •",this_drug,end="")
+            else:    
+                print(dCount," •",this_drug,end="")
+            dCount += 1
+
+
+
+# showList(True)
